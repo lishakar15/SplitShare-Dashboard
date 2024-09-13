6cec159d47ca031d7b5dec17f49608bc8f7618f2
Lishakar <lishakarj@gmail.com>
2024-09-14 01:46:47 +0530

    Manual Split amount validataion implemented

--- src/Expense/SplitCards/ManualSplitCard.js
+++ src/Expense/SplitCards/ManualSplitCard.js
@@ -1,9 +1,10 @@
-import { Grid } from "@mui/material";
-import React, { useEffect } from "react";
+import { Grid,Box,Typography } from "@mui/material";
+import React, { useEffect,useState } from "react";
 import UserDataCard from "../../UserDataCard";
 import CustomSplitInput from "./CustomSplitInput";
 
 const ManualSplitCard = ({ splitList, setSplitList, totalAmount }) => {
+  const [totalSplitAmount,setTotalSplitAmount] = useState(0);
   useEffect(() => {
     setSplitList(
       splitList.map((user) => {
@@ -23,12 +24,48 @@ const ManualSplitCard = ({ splitList, setSplitList, totalAmount }) => {
     setSplitList(updatedSplitList);
     event.target.value=amountVal;
   };
+  useEffect(()=>
+  {
+    const splitAmountSum = splitList.reduce((sum,user)=>{
+      return sum+user.splitAmount;
+    },0);
+    setTotalSplitAmount(splitAmountSum);
+  },[splitList]);
 
   const handleUserCardDelete = (deleteUserID) => {
     setSplitList(splitList.filter((user) => user.userId !== deleteUserID));
   };
   return (
     <>
+    <Grid item xs={12}>
+        <Box
+          sx={{
+            textAlign: "center",
+            backgroundColor: "f9fafb",
+            border: "1px solid lightgray",
+            borderRadius: "11px",
+            py: 1,
+          }}
+        >
+          <Typography>
+            Specify the percentages that's fair for your situation.
+          </Typography>
+          <Typography sx={{ fontWeight: "bold" }}>
+          Total Adjustments: ₹{totalSplitAmount !== null && totalSplitAmount !== undefined ? totalSplitAmount.toFixed(2) : 0}
+          </Typography>
+          {totalSplitAmount < totalAmount ? (
+            <Typography sx={{ color: "red", fontWeight: "bold" }}>
+              Under by : ₹{(totalAmount-totalSplitAmount).toFixed(2)}
+            </Typography>
+          ) : (
+            totalSplitAmount > totalAmount ?
+            (<Typography sx={{ color: "red", fontWeight: "bold" }}>
+              Over by : ₹{ (totalSplitAmount- totalAmount).toFixed(2)}
+            </Typography>)
+            :null
+          )}
+        </Box>
+      </Grid>
       {splitList &&
         splitList.map((user) => (
           <Grid item xs={12} md={6} key={user.userId}>
