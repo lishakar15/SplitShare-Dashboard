db1ebbd20536f6784484a4e2de08d2c9ae3e5ab4
Lishakar <lishakarj@gmail.com>
2024-09-16 01:52:15 +0530

    Component Layout changed to have Sidebar, searchbar for every URL

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import './App.css';
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Home from './Home';
-import Activity from './Activity';
-import Friends from './Friends';
-import Groups from './Groups';
-import Expense from './Expenses';
-import PageNotFound from './PageNotFound';
-
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Activity from "./Activity";
+import Friends from "./Friends";
+import Groups from "./Groups";
+import Expense from "./Expenses";
+import PageNotFound from "./PageNotFound";
+import SideNavBar from "./SideNavBar";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+import { useMediaQuery } from "@mui/material";
+import SearchBar from "./SearchBar";
 
 function App() {
-
+  const isSmallScreen = useMediaQuery("(max-width:1200px)");
 
   return (
     <>
-    <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path="/activities" element={<Activity/>}/>
-    <Route path="/friends" element={<Friends/>}/>
-    <Route path="/groups" element={<Groups/>} />
-    <Route path="/expenses" element={<Expense/>} />
-    <Route path="*" element={<PageNotFound/>}/>
-  </Routes>
+      <Box sx={{ flexGrow: 1, m: 1 }}>
+        <Grid container sx={{ height: "100vh" }}>
+          {isSmallScreen ? null : (
+            <Grid size={2}>
+              <SideNavBar />
+            </Grid>
+          )}
+          <Grid size={{ sm: 12, md: 12, lg: 10 }}>
+            <Box container sx={{ ml: "15px" }}>
+              <SearchBar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/activities" element={<Activity />} />
+                <Route path="/friends" element={<Friends />} />
+                <Route path="/groups" element={<Groups />} />
+                <Route path="/expenses" element={<Expense />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,17 @@
-import React from 'react';
-import SideNavBar from './SideNavBar';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid2';
-import MainSection from './MainSection';
-import Divider from '@mui/material/Divider';
-import { useMediaQuery } from '@mui/material';
+import React from "react";
+import BalanceBoard from "./Balance/BalanceBoard";
+import BalanceAppBar from "./Balance/BalanceAppBar";
+import BalanceCard from "./Balance/BalanceCard";
 
+const Home = () => {
 
-
-const Home = () =>{
-
-    const isSmallScreen = useMediaQuery('(max-width:1200px)');
-      
-    return(
-        <>
-            <Box sx={{ flexGrow: 1,m:1}}>
-            <Grid container sx={{height:"100vh"}}>
-            {isSmallScreen? null : 
-                    <Grid size={2}>
-                    <SideNavBar/>
-                </Grid>
-            }
-                <Grid size={{sm:12,md:12,lg:10}}>
-                    <MainSection/>
-                </Grid>
-            </Grid>
-          </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <BalanceBoard />
+      <BalanceAppBar />
+      <BalanceCard />
+    </>
+  );
+};
 
 export default Home;
