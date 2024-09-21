d6d8382c9931aef0676e9d45bfcb8fb9cc8ffd65
Lishakar <lishakarj@gmail.com>
2024-09-21 22:14:57 +0530

    Payment Accordian in Settlement screen

---
+++ src/data/SettlementData.js
@@ -0,0 +1,30 @@
+export const SETTLEMENT_DATA = [
+    {
+        "settlementId" : 1,
+        "groupId" : 1,
+        "paidBy" : 101,
+        "paidByUserName" : "Lisha",
+        "paidTo" : 103,
+        "paidToUserName" : "Sovon",
+        "createdBy" : 101,
+        "modifiedBy" : null,
+        "amountPaid" : 	33.33,
+        "paymentMethod" : "Cash",
+        "settlementDate" : "2024-07-21T12:34:56Z",
+        "lastUpdateDate" : null
+    },
+    {
+        "settlementId" : 2,
+        "groupId" : 1,
+        "paidBy" : 102,
+        "paidByUserName" : "Jack",
+        "paidTo" : 104,
+        "paidToUserName" : "Andy",
+        "createdBy" : 101,
+        "modifiedBy" : null,
+        "amountPaid" : 	33.33,
+        "paymentMethod" : "Cash",
+        "settlementDate" : "2024-07-21T12:34:56Z",
+        "lastUpdateDate" : null
+    }
+] 
